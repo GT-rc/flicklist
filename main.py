cbcda61960cb471aadacb2ebc1cd18d9349a0650
Glynnis Thomas <glynnisthomas@gmail.com>
2017-01-19 20:32:25 -0600

    Added Tomorrow's Movie feature from Studio

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
-        # TODO: make a list with at least 5 movie titles
+        movie_list = ["The Hitchhiker's Guide to the Galaxy", "MegaMind", "The Wizard of Oz", "Star Wars", "Harry Potter"]
 
-        # TODO: randomly choose one of the movies, and return it
+        movie = movie_list[random.randrange(len(movie_list))]
 
-        return "The Big Lebowski"
+        return movie
 
     def get(self):
         # choose a movie by invoking our new function
@@ -18,10 +19,12 @@ class Index(webapp2.RequestHandler):
         content = "<h1>Movie of the Day</h1>"
         content += "<p>" + movie + "</p>"
 
-        # TODO: pick a different random movie, and display it under
-        # the heading "<h1>Tommorrow's Movie</h1>"
+        content_next = "<h1>Movie for Tommorrow</h1>"
+        content_next += "<p>{movie_a}".format(movie_a=self.getRandomMovie())
+
 
         self.response.write(content)
+        self.response.write(content_next)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
